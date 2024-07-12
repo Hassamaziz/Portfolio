3110c44c6005dc84a02d3b2805cc337970bab66c
hassamaziz <hassamaziz001@gmail.com>
2024-07-12 23:29:20 +0500

    done

--- my-portfolio/src/components/home/Home.jsx
+++ my-portfolio/src/components/home/Home.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import "./home.css";
-import Me from "../../assets/HASSAM.png"
+import Me from "../../assets/profile.png";
 import HeaderSocials from './HeaderSocials';
 import ScrollDown from './ScrollDown';
 import Shapes from './Shapes';
-
+import { ReactTyped as Typed } from 'react-typed'; 
 
 const Home = () => {
   return (
     <section className="home container" id="home">
-    <div className="intro">
-    <img src={Me} width={150} alt="" className="home__img" />
-    <h1 className="home__name">Hassam Aziz</h1>
-    <span className="home__education">I'm a Front-End Developer</span>
+      <div className="intro">
+        <img src={Me} width={180} style={{borderRadius:'50%'}} alt="" className="home__img" />
+        <h1 className="home__name">Hassam Aziz</h1>
+        <span className="home__education">
+          I'm&nbsp;
+          <Typed
+            strings={["a Front-End Developer", "a Programmer", "a Problem Solver", "an Overthinker"]}
+            typeSpeed={40}
+            backSpeed={50}
+            loop
+          />
+        </span>
 
-    <HeaderSocials/>
+        <HeaderSocials/>
 
-    <a href="#contact" className="btn">Hire Me</a>
+        <a href="#contact" className="btn">Hire Me</a>
 
-    <ScrollDown/>
-
-    </div>
-    
-      
-        
+        <ScrollDown/>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
