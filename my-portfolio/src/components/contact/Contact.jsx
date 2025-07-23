7996ede2d7ce52f714fb41711d6c882ffb48c8af
hassamaziz <hassamaziz001@gmail.com>
2025-07-23 18:11:31 +0500

    email.js tidy up

--- my-portfolio/src/components/contact/Contact.jsx
+++ my-portfolio/src/components/contact/Contact.jsx
@@ -14,9 +14,9 @@ const Contact = () => {
   });
 
   useEffect(() => {
-    console.log("Service ID:", process.env.REACT_APP_EMAILJS_SERVICE_ID);
-    console.log("Template ID:", process.env.REACT_APP_EMAILJS_TEMPLATE_ID);
-    console.log("Public Key:", process.env.REACT_APP_EMAILJS_PUBLIC_KEY);
+    // console.log("Service ID:", process.env.REACT_APP_EMAILJS_SERVICE_ID);
+    // console.log("Template ID:", process.env.REACT_APP_EMAILJS_TEMPLATE_ID);
+    // console.log("Public Key:", process.env.REACT_APP_EMAILJS_PUBLIC_KEY);
   }, []);
   
 
